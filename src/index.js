cb950719562f566383653220394d57d69baf1bd8
younghwani <younghwankim624@gmail.com>
2021-03-13 19:53:07 +0900

    #2.2 useClick

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const useTitle = (initialTitle) => {
-	const [title, setTitle] = useState(initialTitle);
-	const updateTitle = () => {
-		const htmlTitle = document.querySelector("title");
-		htmlTitle.innerText = title;
-	};
-	useEffect(updateTitle, [title]);
-	return setTitle;
+const useClick = (onClick) => {
+	const element = useRef();
+	useEffect(() => {
+		//componentDidMount()
+		if (element.current && typeof onClick === "function") {
+			element.current.addEventListener("click", onClick);
+		}
+		//componentWillUnmount()
+		return () => {
+			if (element.current && typeof onClick === "function") {
+				element.current.removeEventListener("click", onClick);
+			}
+		};
+	}, []); //dependencyList를 통해 componentDidUpdate()일 경우 호출 안함.
+	return element;
 };
 
 const App = () => {
-	const titleUpdater = useTitle("Loading...");
-	setTimeout(() => titleUpdater("Home"), 3000);
+	const sayHello = () => console.log("say hello!");
+	const title = useClick(sayHello);
 	return (
 		<div className="App">
-			<h1>Hello</h1>
+			<h1 ref={title}>Hello</h1>
 		</div>
 	);
 };
