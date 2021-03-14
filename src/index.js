fd229c380116949f2d2f1a5e38021272919ee53b
younghwani <younghwankim624@gmail.com>
2021-03-14 19:51:36 +0900

    #2.7 useNotification

--- src/index.js
+++ src/index.js
@@ -1,59 +1,33 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const useFullscreen = (callback) => {
-	const [fullCheck, setFullCheck] = useState(false);
-	const element = useRef();
-	const runCb = (isFull) => {
-		if (callback && typeof callback === "function") {
-			callback(isFull);
+const useNotification = (title, options) => {
+	if (!("Notification" in window)) {
+		return;
+	}
+	const fireNotif = () => {
+		if (Notification.permission !== "granted") {
+			Notification.requestPermission().then((permission) => {
+				if (permission === "granted") {
+					new Notification(title, options);
+				} else {
+					return;
+				}
+			});
+		} else {
+			new Notification(title, options);
 		}
 	};
-	const triggerFull = () => {
-		if (element.current) {
-			if (element.current.requestFullscreen) {
-				element.current.requestFullscreen();
-			} else if (element.current.mozRequestFullScreen) {
-				element.current.mozRequestFullScreen();
-			} else if (element.current.webkitRequestFullscreen) {
-				element.current.webkitRequestFullscreen();
-			} else if (element.current.msRequestFullscreen) {
-				element.current.msRequestFullscreen();
-			}
-			runCb(true);
-			setFullCheck(true);
-		}
-	};
-	const exitFull = () => {
-		if (document.exitFullscreen) {
-			document.exitFullscreen();
-		} else if (document.mozCancelFullScreen) {
-			document.mozCancelFullScreen();
-		} else if (document.webkitExitFullscreen) {
-			document.webkitExitFullscreen();
-		} else if (document.msExitFullscreen) {
-			document.msExitFullscreen();
-		}
-		runCb(false);
-		setFullCheck(false);
-	};
-	return { element, triggerFull, exitFull, fullCheck };
 };
 
 const App = () => {
-	const onFullS = (isFull) => {
-		console.log(isFull ? "We are full" : "We are small");
-	};
-	const { element, triggerFull, exitFull, fullCheck } = useFullscreen(onFullS);
+	const triggerNotif = useNotification("Can I steel your hearts?", {
+		body: "I am a Student!",
+	});
 	return (
 		<div className="App" style={{ height: "1000vh" }}>
-			<div ref={element}>
-				<img src="https://lh3.googleusercontent.com/proxy/QINlJByxnBo0hQz4j9eKCCzNOkgKlC51o2aBgmNHrngs0VnoeQwP3H5DNRRI7maEAc69bwN_K1XW_wTUnEgZRiU4pdUVoo45f7C8i3rxzopUIoJmRsXFzi-ddP1M8ZM3BQNF1n4TfFkKuXIVt5E7xg6dfyAAHnYfpFdjb7VfFmRhO8n1lAolpj6RVO-fHV6f03beVrgChkcuShKaXYOZfC4HTBPTtRStszTpaPF3MEslbjdFI7MLPrZMhuWq2qH-pm3-EOTjytW_nfzyhsiCjlf3r0xzuTaRbAMe9ulHPMteg5_qVOFeNtmqbULkWCINXnnJuLwgWF9dY18OJ0G-MLfJemab6667GVEDyaah_Uom"></img>
-				<br></br>
-				<button onClick={exitFull} disabled={!fullCheck}>Exit fullscreen</button>
-			</div>
-			<button onClick={triggerFull}>Make fullscreen</button>
+			<button onClick={triggerNotif}>Hello</button>
 		</div>
 	);
 };
