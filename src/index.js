a31e5d020eaaa51dfee000d0e406950c7ce92eff
younghwani <younghwankim624@gmail.com>
2021-03-14 20:50:13 +0900

    #2.8 useAxios

--- src/index.js
+++ src/index.js
@@ -1,33 +1,21 @@
 import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-
-const useNotification = (title, options) => {
-	if (!("Notification" in window)) {
-		return;
-	}
-	const fireNotif = () => {
-		if (Notification.permission !== "granted") {
-			Notification.requestPermission().then((permission) => {
-				if (permission === "granted") {
-					new Notification(title, options);
-				} else {
-					return;
-				}
-			});
-		} else {
-			new Notification(title, options);
-		}
-	};
-};
+import useAxios from "./useAxios";
 
 const App = () => {
-	const triggerNotif = useNotification("Can I steel your hearts?", {
-		body: "I am a Student!",
+	const { loading, data, error, refetch } = useAxios({
+		url:
+			"https://cors-anywhere.herokuapp.com/https://yts.am/api/v2/list_movies.json",
 	});
+	console.log(
+		`Loading: ${loading}\nError: ${error}\nData: ${JSON.stringify(data)}`
+	);
 	return (
 		<div className="App" style={{ height: "1000vh" }}>
-			<button onClick={triggerNotif}>Hello</button>
+			<h1>{data && data.status}</h1>
+			<h2>{loading && "Loading"}</h2>
+			<button onClick={refetch}>Refetch</button>
 		</div>
 	);
 };
